9c940a03eb0612c17ef465409feb09dd4685a595
Flavio <l.flavio@aluno.ifsp.edu.br>
2024-02-15 15:27:54 -0300

    #criando readme

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.boasvindas}>ola mundo react native</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boasvindas: {
+    color: '#568975',
+    backgroundColor: '#DDDDDD',
+    fontSize: 30
+  }
 });
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Comando Uteis 
+
+### Criar um novo projeto React NAtive com Expo 
+
+...
+npx create-expo-app --template 
+...
+
+### Testar no celular com Tunnel 
+
+1) baixar p app Expo Go 
+
+2) Instalar o tunnel 
+...
+expo start --tunnel 
+...
